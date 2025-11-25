fa246e6d610dda500c658bc391c1644aaf032ab4
Jan Dvorak <jan.dvorak@oddin.gg>
2025-11-25 14:48:21 +0100

    Preview changes

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+def foo(a, b):
+    print(a * b)
+
 
 def main() -> None:
     print("Hello World")
+    print("Hello World")
+    print("Hello World")
+    print("Hello World")
+    foo(1, 2)
+
 
 
 if __name__ == '__main__':
